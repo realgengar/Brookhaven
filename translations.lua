c5d4d9b9fdf45d5fed9487993782adefc0091ed1
realgengar <pedrotrixoia@gmail.com>
2025-07-31 13:19:24 -0300

    Create translations.lua

---
+++ translations.lua
@@ -0,0 +1,17 @@
+{
+  "en": {
+    "Hello": "Hello",
+    "Welcome": "Welcome to the game!",
+    "Play": "Play"
+  },
+  "pt": {
+    "Hello": "Olá",
+    "Welcome": "Bem-vindo ao jogo!",
+    "Play": "Jogar"
+  },
+  "es": {
+    "Hello": "Hola",
+    "Welcome": "¡Bienvenido al juego!",
+    "Play": "Jugar"
+  }
+}
